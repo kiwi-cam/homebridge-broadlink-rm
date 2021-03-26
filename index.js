bd2b183c2e927457bfe74b977c3d093da09805b3
Cameron <32912464+kiwi-cam@users.noreply.github.com>
2021-03-26 15:42:36 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const BroadlinkRMPlatform = require('./platform');
 const compareVersions = require('compare-versions');
+const fakegatoHistory = require( 'fakegato-history');
 
 module.exports = (homebridge) => {
   if (compareVersions("0.4.47", homebridge.serverVersion) > 0) {
     console.log(`[Broadlink RM] The plugin homebridge-broadlink-rm requires HomeBridge v0.4.47 or higher! You have: ${homebridge.serverVersion}.`);
     process.exit(1);
   }
-  global.FakeGatoHistoryService = require('fakegato-history')(homebridge);
+  
+  HistoryService = fakegatoHistory( homebridge );
   
   global.Service = homebridge.hap.Service;
   global.Characteristic = homebridge.hap.Characteristic;
