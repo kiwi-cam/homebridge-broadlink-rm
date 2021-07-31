032b1085be3a0b7c14d84e242bf25dc40a16e3c7
Dave Nicolson <david.nicolson@gmail.com>
2021-07-31 14:38:47 +0200

    Update repo name and owner

--- helpers/checkForUpdates.js
+++ helpers/checkForUpdates.js
@@ -12,7 +12,7 @@ const checkForUpdates = () => {
   versionCheck (options, (error, update) => { 
     // if (error) throw error;
     if (update) { 
-      console.log(`\x1b[32m[UPDATE AVAILABLE] \x1b[0mVersion ${update.name} of homebridge-broadlink-rm-tv is available. The release notes can be found here: \x1b[4mhttps://github.com/lprhodes/homebridge-broadlink-rm/releases/\x1b[0m`);
+      console.log(`\x1b[32m[UPDATE AVAILABLE] \x1b[0mVersion ${update.name} of Homebridge Broadlink RM Pro is available. The release notes can be found here: \x1b[4mhttps://github.com/${options.owner}/homebridge-broadlink-rm/releases/\x1b[0m`);
     }
   });
 }
